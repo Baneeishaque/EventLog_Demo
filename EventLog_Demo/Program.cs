048d401a4a73785b06f2b50a161af177af2dd987
Banee Ishaque K <baneeishaque@gmail.com>
2020-07-24 02:21:38 +0530

    Update Program.cs

--- EventLog_Demo/Program.cs
+++ EventLog_Demo/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 
@@ -9,6 +10,19 @@ namespace EventLog_Demo
     {
         static void Main(string[] args)
         {
+            string sSource;
+            string sLog;
+            string sEvent;
+
+            sSource = "dotNET Sample App";
+            sLog = "Application";
+            sEvent = "Sample Event";
+
+            if (!EventLog.SourceExists(sSource))
+                EventLog.CreateEventSource(sSource, sLog);
+
+            EventLog.WriteEntry(sSource, sEvent);
+            EventLog.WriteEntry(sSource, sEvent,EventLogEntryType.Warning, 234);
         }
     }
 }
